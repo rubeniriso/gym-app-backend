71e40975a1e4fa5c10def8a1749b0cd5f0559b33
rubeniriso <rubenirisoe@gmail.com>
2024-05-12 10:53:14 +0200

    [#12] - Model with merge in case we need it

--- models/TrainingDay.model.js
+++ models/TrainingDay.model.js
@@ -8,7 +8,8 @@ const findById = async (id) => {
 };
 
 const findByWeekId = async (id) => {
-  const query = "SELECT * from trainingday WHERE week_id = $1 ORDER BY order_ ASC";
+  const query =
+    "SELECT * from trainingday WHERE week_id = $1 ORDER BY order_ ASC";
   const values = [id];
   const result = await pool.query(query, values);
   return result;
@@ -16,7 +17,8 @@ const findByWeekId = async (id) => {
 
 const deleteById = async (id) => {
   // TODO: tener en cuenta posible constraint con el id de la session borrada en trainingdayexercise.
-  const query = "DELETE from trainingday WHERE trainingday_id = $1 RETURNING *;";
+  const query =
+    "DELETE from trainingday WHERE trainingday_id = $1 RETURNING *;";
   const values = [id];
   const result = await pool.query(query, values);
   return result;
@@ -33,9 +35,7 @@ const create = async (week_id, trainingDayData) => {
 };
 
 const update = async (trainingday_id, trainingDayData) => {
-  const values = ["name", "description"].map(
-    (key) => trainingDayData[key]
-  );
+  const values = ["name", "description"].map((key) => trainingDayData[key]);
   values.push(trainingday_id);
   const query = `
       UPDATE trainingday
@@ -47,11 +47,58 @@ const update = async (trainingday_id, trainingDayData) => {
     `;
   return await pool.query(query, values);
 };
+const updateExercises = async (trainingday_id, trainingDayExerciseData) => {
+  try {
+    await pool.query("BEGIN");
+    console.log("got here");
+    // Construct the values string for all exercises
+    const values = trainingDayExerciseData.exerciseData
+      .map(
+        (exerciseData) =>
+          `(${exerciseData.trainingDayExerciseId}, 
+        ${exerciseData}, 
+        ${exerciseData.bodyPart}, 
+        ${exerciseData.muscle}, 
+        ${exerciseData.exercise}, 
+        ${exerciseData.sets}, 
+        ${exerciseData.reps}, 
+        ${exerciseData.weight}, 
+        ${exerciseData.rir})`
+      )
+      .join(", ");
+
+    // Merge operation using a single SQL query
+    await pool.query(`
+      MERGE INTO trainingdayexercise AS target
+      USING (VALUES ${values}) AS source (trainingdayexercise_id, bodypart_id, muscle_id, exercise_id, sets, reps, weight, rir)
+      ON target.trainingdayexercise_id = source.trainingdayexercise_id
+      WHEN MATCHED THEN
+        UPDATE SET
+          bodypart_id = source.bodypart_id,
+          muscle_id = source.muscle_id,
+          exercise_id = source.exercise_id,
+          sets = source.sets,
+          reps = source.reps,
+          weight = source.weight,
+          rir = source.rir
+      WHEN NOT MATCHED THEN
+        INSERT (trainingdayexercise_id, trainingday_id, bodypart_id, muscle_id, exercise_id, sets, reps, weight, rir)
+        VALUES (source.trainingdayexercise_id, ${trainingday_id}, source.bodypart_id, source.muscle_id, source.exercise_id, source.sets, source.reps, source.weight, source.rir);
+    `);
+
+    await pool.query("COMMIT");
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    throw error;
+  }
+  return await pool.query(query, values);
+};
 
 export const TrainingDayModel = {
   findById,
   findByWeekId,
   deleteById,
   create,
-  update
+  update,
+  updateExercises,
 };
